05ba2000b2c2c01347d6118493659e03d4b8a4b8
GroupA-lab <perfectstrenger9@gmail.com>
2022-08-13 15:04:29 +0300

    user changes 2

--- library/urls.py
+++ library/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from .views import index, Add_book_view, Add_students_view, view_books, view_students, issue_book, view_issued_book, profile, edit_profile, registerUser, loginUser
+
 
 urlpatterns = [
     path('', views.home, name="home"),
@@ -12,7 +14,7 @@ urlpatterns = [
     path("view_issued_book/", views.view_issued_book, name="view_issued_book"),
     path("profile/", views.profile, name="profile"),
     path("edit_profile/", views.edit_profile, name="edit_profile"),
-    path('register/', views.registeruser, name="register"),
+    path('register/', views.registerUser, name="register"),
     #path("change_password/", views.change_password, name="change_password"),
     path('login/', views.loginUser, name="login"),
     #path("admin_login/", views.admin_login, name="admin_login"),
